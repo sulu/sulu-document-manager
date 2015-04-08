sulu/sulu

Added document manager
Added gitignore

NodeMnaager tests

Added InstantaitorTests

Added registrator tests

Added tests for document registry

Added NodeCreatorSubscriber

AutoName subscriber

Added DocumentManagerBundle

DocumentManagerBundle

Added general subscriber

Added query events

Query

Added core dependency

Query

Added urlizer dep.

Added TimestampBehavior

Blame behavior

Basic DM working

src/Sulu/Component/DocumentManager/Subscriber/Core/InstantiatorSubscriber.php
@@ -1,0 +1,136 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\DocumentManager\Subscriber\Core;
+
+use Sulu\Component\DocumentManager\Document\UndefinedDocument;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Sulu\Component\DocumentManager\MetadataFactory;
+use Sulu\Component\DocumentManager\Event\HydrateEvent;
+use Sulu\Component\DocumentManager\Event\CreateEvent;
+use PHPCR\NodeInterface;
+use Sulu\Component\DocumentManager\Metadata;
+use Sulu\Component\DocumentManager\Events;
+
+/**
+ * Responsible for intantiating documents from PHPCR nodes and
+ * setting the document in the event so that other listeners can
+ * take further actions (such as hydrating it for example).
+ *
+ * NOTE: This should always be the first thing to be called
+ */
+class InstantiatorSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var MetadataFactory
+     */
+    private $metadataFactory;
+
+    /**
+     * @param MetadataFactory $metadataFactory
+     */
+    public function __construct(MetadataFactory $metadataFactory)
+    {
+        $this->metadataFactory = $metadataFactory;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            Events::HYDRATE => array('handleHydrate', 500),
+            Events::CREATE => array('handleCreate', 500),
+        );
+    }
+
+    /**
+     * @param HydrateEvent $event
+     */
+    public function handleHydrate(HydrateEvent $event)
+    {
+        // don't need to instantiate the document if it is already existing.
+        if ($event->hasDocument()) {
+            return;
+        }
+
+        $node = $event->getNode();
+
+        $document = $this->getDocumentFromNode($node);
+        $event->setDocument($document);
+    }
+
+    /**
+     * @param mixed $event
+     */
+    public function handleCreate(CreateEvent $event)
+    {
+        $metadata = $this->metadataFactory->getMetadataForAlias($event->getAlias());
+        $document = $this->instantiateFromMetadata($metadata);
+        $event->setDocument($document);
+    }
+
+    /**
+     * Instantiate a new document. The class is determined from
+     * the mixins present in the PHPCR node for legacy reasons.
+     *
+     * TODO: We need to migrate to using the primary node type.
+     *
+     * @param NodeInterface $node
+     */
+    private function getDocumentFromNode(NodeInterface $node)
+    {
+        if (!$node->hasProperty('jcr:mixinTypes')) {
+            return $this->createUndefined();
+        }
+
+        $mixinTypes = $node->getPropertyValue('jcr:mixinTypes');
+        $metadata = null;
+
+        foreach ($mixinTypes as $mixinType) {
+            if (true == $this->metadataFactory->hasMetadataForPhpcrType($mixinType)) {
+                $metadata = $this->metadataFactory->getMetadataForPhpcrType($mixinType);
+                break;
+            }
+        }
+
+        if (null === $metadata) {
+            return $this->createUndefined();
+        }
+
+        return $this->instantiateFromMetadata($metadata);
+    }
+
+    private function instantiateFromMetadata(Metadata $metadata)
+    {
+        $class = $metadata->getClass();
+
+        if (!class_exists($class)) {
+            throw new \RuntimeException(sprintf(
+                'Document class "%s" does not exist', $class
+            ));
+        }
+
+        $document = new $class;
+
+        return $document;
+    }
+
+    /**
+     * Creates an undefined document which represents a non-managed phpcr node
+     *
+     * @return UndefinedDocument
+     */
+    private function createUndefined()
+    {
+        return new UndefinedDocument();
+    }
+}

src/Sulu/Component/DocumentManager/Tests/Unit/Subscriber/Core/InstantiatorSubscriberTest.php
@@ -1,0 +1,108 @@
+<?php
+
+namespace Sulu\Component\DocumentManager\Tests\Unit\Subscriber\Core;
+
+use Sulu\Component\DocumentManager\MetadataFactory;
+use Sulu\Component\DocumentManager\Metadata;
+use PHPCR\NodeInterface;
+use Sulu\Component\DocumentManager\Event\CreateEvent;
+use Sulu\Component\DocumentManager\Event\HydrateEvent;
+use Sulu\Component\DocumentManager\Subscriber\Core\InstantiatorSubscriber;
+use Prophecy\Argument;
+use Sulu\Component\DocumentManager\Document\UndefinedDocument;
+
+class InstantiatorSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    const MIXIN_TYPES = 'jcr:mixinTypes';
+    const PHPCR_TYPE = 'sulu:foobar';
+    const ALIAS = 'alias';
+
+    private $subscriber;
+
+    public function setUp()
+    {
+        $this->metadataFactory = $this->prophesize(MetadataFactory::class);
+        $this->subscriber = new InstantiatorSubscriber(
+            $this->metadataFactory->reveal()
+        );
+
+        $this->metadata = $this->prophesize(Metadata::class);
+        $this->hydrateEvent =  $this->prophesize(HydrateEvent::class);
+        $this->createEvent = $this->prophesize(CreateEvent::class);
+        $this->node = $this->prophesize(NodeInterface::class);
+    }
+
+
+    /**
+     * It should create a document for a managed PHPCR node
+     */
+    public function testHandleHydrate()
+    {
+        $this->hydrateEvent->hasDocument()->willReturn(false);
+        $this->hydrateEvent->getNode()->willReturn($this->node->reveal());
+        $this->node->hasProperty(self::MIXIN_TYPES)->willReturn(true);
+        $this->node->getPropertyValue(self::MIXIN_TYPES)->willReturn(array(
+            self::PHPCR_TYPE
+        ));
+        $this->metadataFactory->hasMetadataForPhpcrType(self::PHPCR_TYPE)->willReturn(true);
+        $this->metadataFactory->getMetadataForPhpcrType(self::PHPCR_TYPE)->willReturn(
+            $this->metadata->reveal()
+        );
+        $this->metadata->getClass()->willReturn('\stdClass');
+        $this->hydrateEvent->setDocument(Argument::type('stdClass'))->shouldBeCalled();
+
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+    }
+
+    /**
+     * It should create an UndefinedDocument for a not-managed PHPCR node
+     */
+    public function testHandleHydrateNotManaged()
+    {
+        $this->hydrateEvent->hasDocument()->willReturn(false);
+        $this->hydrateEvent->getNode()->willReturn($this->node->reveal());
+        $this->node->hasProperty(self::MIXIN_TYPES)->willReturn(false);
+        $this->hydrateEvent->setDocument(Argument::type(UndefinedDocument::class))->shouldBeCalled();
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+    }
+
+    /**
+     * It should create an UndefinedDocument for an not-mapped PHPCR node
+     */
+    public function testHandleHydrateNotMapped()
+    {
+        $this->hydrateEvent->hasDocument()->willReturn(false);
+        $this->hydrateEvent->getNode()->willReturn($this->node->reveal());
+        $this->node->hasProperty(self::MIXIN_TYPES)->willReturn(true);
+        $this->node->getPropertyValue(self::MIXIN_TYPES)->willReturn(array(
+            self::PHPCR_TYPE
+        ));
+        $this->metadataFactory->hasMetadataForPhpcrType(self::PHPCR_TYPE)->willReturn(false);
+        $this->hydrateEvent->setDocument(Argument::type(UndefinedDocument::class))->shouldBeCalled();
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+    }
+
+    /**
+     * If the document has already been set, do nothing
+     */
+    public function testHandleHydrateDocumentAlreadySet()
+    {
+        $this->hydrateEvent->hasDocument()->willReturn(true);
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+    }
+
+    /**
+     * It should create a new document
+     */
+    public function testHandleCreate()
+    {
+        $this->createEvent->getAlias()->willReturn(self::ALIAS);
+        $this->metadataFactory->getMetadataForAlias(self::ALIAS)->willReturn(
+            $this->metadata->reveal()
+        );
+        $this->metadata->getClass()->willReturn('\stdClass');
+        $this->createEvent->setDocument(Argument::type('stdClass'))->shouldBeCalled();
+
+        $this->subscriber->handleCreate($this->createEvent->reveal());
+    }
+}

sulu/sulu-document-manager

Subscriber/Core/InstantiatorSubscriber.php
@@ -1,0 +1,136 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\DocumentManager\Subscriber\Core;
+
+use Sulu\Component\DocumentManager\Document\UnknownDocument;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Sulu\Component\DocumentManager\MetadataFactory;
+use Sulu\Component\DocumentManager\Event\HydrateEvent;
+use Sulu\Component\DocumentManager\Event\CreateEvent;
+use PHPCR\NodeInterface;
+use Sulu\Component\DocumentManager\Metadata;
+use Sulu\Component\DocumentManager\Events;
+
+/**
+ * Responsible for intantiating documents from PHPCR nodes and
+ * setting the document in the event so that other listeners can
+ * take further actions (such as hydrating it for example).
+ *
+ * NOTE: This should always be the first thing to be called
+ */
+class InstantiatorSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var MetadataFactory
+     */
+    private $metadataFactory;
+
+    /**
+     * @param MetadataFactory $metadataFactory
+     */
+    public function __construct(MetadataFactory $metadataFactory)
+    {
+        $this->metadataFactory = $metadataFactory;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            Events::HYDRATE => array('handleHydrate', 500),
+            Events::CREATE => array('handleCreate', 500),
+        );
+    }
+
+    /**
+     * @param HydrateEvent $event
+     */
+    public function handleHydrate(HydrateEvent $event)
+    {
+        // don't need to instantiate the document if it is already existing.
+        if ($event->hasDocument()) {
+            return;
+        }
+
+        $node = $event->getNode();
+
+        $document = $this->getDocumentFromNode($node);
+        $event->setDocument($document);
+    }
+
+    /**
+     * @param mixed $event
+     */
+    public function handleCreate(CreateEvent $event)
+    {
+        $metadata = $this->metadataFactory->getMetadataForAlias($event->getAlias());
+        $document = $this->instantiateFromMetadata($metadata);
+        $event->setDocument($document);
+    }
+
+    /**
+     * Instantiate a new document. The class is determined from
+     * the mixins present in the PHPCR node for legacy reasons.
+     *
+     * TODO: We need to migrate to using the primary node type.
+     *
+     * @param NodeInterface $node
+     */
+    private function getDocumentFromNode(NodeInterface $node)
+    {
+        if (!$node->hasProperty('jcr:mixinTypes')) {
+            return $this->createUndefined();
+        }
+
+        $mixinTypes = $node->getPropertyValue('jcr:mixinTypes');
+        $metadata = null;
+
+        foreach ($mixinTypes as $mixinType) {
+            if (true == $this->metadataFactory->hasMetadataForPhpcrType($mixinType)) {
+                $metadata = $this->metadataFactory->getMetadataForPhpcrType($mixinType);
+                break;
+            }
+        }
+
+        if (null === $metadata) {
+            return $this->createUndefined();
+        }
+
+        return $this->instantiateFromMetadata($metadata);
+    }
+
+    private function instantiateFromMetadata(Metadata $metadata)
+    {
+        $class = $metadata->getClass();
+
+        if (!class_exists($class)) {
+            throw new \RuntimeException(sprintf(
+                'Document class "%s" does not exist', $class
+            ));
+        }
+
+        $document = new $class;
+
+        return $document;
+    }
+
+    /**
+     * Creates an undefined document which represents a non-managed phpcr node
+     *
+     * @return UnknownDocument
+     */
+    private function createUndefined()
+    {
+        return new UnknownDocument();
+    }
+}

Tests/Unit/Subscriber/Core/InstantiatorSubscriberTest.php
@@ -1,0 +1,108 @@
+<?php
+
+namespace Sulu\Component\DocumentManager\Tests\Unit\Subscriber\Core;
+
+use Sulu\Component\DocumentManager\MetadataFactory;
+use Sulu\Component\DocumentManager\Metadata;
+use PHPCR\NodeInterface;
+use Sulu\Component\DocumentManager\Event\CreateEvent;
+use Sulu\Component\DocumentManager\Event\HydrateEvent;
+use Sulu\Component\DocumentManager\Subscriber\Core\InstantiatorSubscriber;
+use Prophecy\Argument;
+use Sulu\Component\DocumentManager\Document\UnknownDocument;
+
+class InstantiatorSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    const MIXIN_TYPES = 'jcr:mixinTypes';
+    const PHPCR_TYPE = 'sulu:foobar';
+    const ALIAS = 'alias';
+
+    private $subscriber;
+
+    public function setUp()
+    {
+        $this->metadataFactory = $this->prophesize(MetadataFactory::class);
+        $this->subscriber = new InstantiatorSubscriber(
+            $this->metadataFactory->reveal()
+        );
+
+        $this->metadata = $this->prophesize(Metadata::class);
+        $this->hydrateEvent =  $this->prophesize(HydrateEvent::class);
+        $this->createEvent = $this->prophesize(CreateEvent::class);
+        $this->node = $this->prophesize(NodeInterface::class);
+    }
+
+
+    /**
+     * It should create a document for a managed PHPCR node
+     */
+    public function testHandleHydrate()
+    {
+        $this->hydrateEvent->hasDocument()->willReturn(false);
+        $this->hydrateEvent->getNode()->willReturn($this->node->reveal());
+        $this->node->hasProperty(self::MIXIN_TYPES)->willReturn(true);
+        $this->node->getPropertyValue(self::MIXIN_TYPES)->willReturn(array(
+            self::PHPCR_TYPE
+        ));
+        $this->metadataFactory->hasMetadataForPhpcrType(self::PHPCR_TYPE)->willReturn(true);
+        $this->metadataFactory->getMetadataForPhpcrType(self::PHPCR_TYPE)->willReturn(
+            $this->metadata->reveal()
+        );
+        $this->metadata->getClass()->willReturn('\stdClass');
+        $this->hydrateEvent->setDocument(Argument::type('stdClass'))->shouldBeCalled();
+
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+    }
+
+    /**
+     * It should create an UnknownDocument for a not-managed PHPCR node
+     */
+    public function testHandleHydrateNotManaged()
+    {
+        $this->hydrateEvent->hasDocument()->willReturn(false);
+        $this->hydrateEvent->getNode()->willReturn($this->node->reveal());
+        $this->node->hasProperty(self::MIXIN_TYPES)->willReturn(false);
+        $this->hydrateEvent->setDocument(Argument::type(UnknownDocument::class))->shouldBeCalled();
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+    }
+
+    /**
+     * It should create an UnknownDocument for an not-mapped PHPCR node
+     */
+    public function testHandleHydrateNotMapped()
+    {
+        $this->hydrateEvent->hasDocument()->willReturn(false);
+        $this->hydrateEvent->getNode()->willReturn($this->node->reveal());
+        $this->node->hasProperty(self::MIXIN_TYPES)->willReturn(true);
+        $this->node->getPropertyValue(self::MIXIN_TYPES)->willReturn(array(
+            self::PHPCR_TYPE
+        ));
+        $this->metadataFactory->hasMetadataForPhpcrType(self::PHPCR_TYPE)->willReturn(false);
+        $this->hydrateEvent->setDocument(Argument::type(UnknownDocument::class))->shouldBeCalled();
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+    }
+
+    /**
+     * If the document has already been set, do nothing
+     */
+    public function testHandleHydrateDocumentAlreadySet()
+    {
+        $this->hydrateEvent->hasDocument()->willReturn(true);
+        $this->subscriber->handleHydrate($this->hydrateEvent->reveal());
+    }
+
+    /**
+     * It should create a new document
+     */
+    public function testHandleCreate()
+    {
+        $this->createEvent->getAlias()->willReturn(self::ALIAS);
+        $this->metadataFactory->getMetadataForAlias(self::ALIAS)->willReturn(
+            $this->metadata->reveal()
+        );
+        $this->metadata->getClass()->willReturn('\stdClass');
+        $this->createEvent->setDocument(Argument::type('stdClass'))->shouldBeCalled();
+
+        $this->subscriber->handleCreate($this->createEvent->reveal());
+    }
+}