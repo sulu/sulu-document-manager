8b898f1604b1111bd400df06d3f261c2f9a570cb
dantleech <dan.t.leech@gmail.com>
2015-04-10 08:28:16 +0200

    Added locale subscriber

---
+++ Subscriber/Behavior/LocaleSubscriber.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Component\DocumentManager\Subscriber\Behavior;
+
+use Sulu\Component\DocumentManager\Events;
+use Sulu\Component\DocumentManager\Behavior\LocaleBehavior;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\Event;
+use Sulu\Component\DocumentManager\Event\AbstractDocumentNodeEvent;
+use Sulu\Component\DocumentManager\DocumentRegistry;
+
+/**
+ * Maps the locale
+ */
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $registry;
+
+    public function __construct(DocumentRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            Events::HYDRATE => 'handleLocale',
+            Events::PERSIST => array('handleLocale', '0'),
+        );
+    }
+
+    public function handleLocale(AbstractDocumentNodeEvent $event)
+    {
+        $document = $event->getDocument();
+
+        if (!$document instanceof LocaleBehavior) {
+            return;
+        }
+
+        $event->getAccessor()->set(
+            'locale',
+            $this->registry->getLocaleForDocument($document)
+        );
+    }
+}
