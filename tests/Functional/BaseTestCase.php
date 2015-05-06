f7481bfb9ba5f677e24663f969942b7d3d3c6190
dantleech <dan.t.leech@gmail.com>
2015-05-06 10:50:21 +0200

    Fix for php<5.6

--- tests/Functional/BaseTestCase.php
+++ tests/Functional/BaseTestCase.php
@@ -18,9 +18,8 @@ use Symfony\Component\EventDispatcher\EventDispatcher;
 abstract class BaseTestCase extends \PHPUnit_Framework_TestCase
 {
     const BASE_NAME = 'test';
-    const BASE_PATH = '/' . self::BASE_NAME;
+    const BASE_PATH = '/test';
 
-    private $session;
     private $container;
 
     protected function initPhpcr()
